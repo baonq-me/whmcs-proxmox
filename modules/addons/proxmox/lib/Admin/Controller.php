194951d3b2642ab4ef374bf2cdd730989371da1c
baonq-me <quocbao747@gmail.com>
2017-12-05 19:15:41 +0700

    Add APIs for item creation

--- modules/addons/proxmox/lib/Admin/Controller.php
+++ modules/addons/proxmox/lib/Admin/Controller.php
@@ -3,6 +3,7 @@
 namespace WHMCS\Module\Addon\Proxmox\Admin;
 
 use WHMCS\Module\Addon\Proxmox\PHPProxmox\PHPProxmox;
+use WHMCS\Module\Addon\Proxmox\WHMCS_Data\WHMCS_Data;
 
 //use Illuminate\Database\Capsule\Manager as Capsule;
 use WHMCS\Database\Capsule;
@@ -16,6 +17,10 @@ use WHMCS\Database\Capsule;
  */
 class Controller {
 
+    public function __construct() {
+        $this->whmcs = new WHMCS_Data();
+    }
+
     /**
      * Index action.
      *
@@ -27,48 +32,52 @@ class Controller {
     {
         $smarty = $vars['smarty'];
 
-        //$version = $vars['version']; // eg. 1.0
-        //$LANG = $vars['_lang']; // an array of the currently loaded language variables
-
         $proxmox = new PHPProxmox($vars);
         $proxmox->buildConfig();
+
+
         $status = implode(" ", $proxmox->getSystemStatus());
+        $pveConnection = intval(strpos($status, 'Could not connect to server'));
 
-        preg_match_all('/pve cpu thread (\d+) loadavg ([\.0-9]+) ([\.0-9]+) ([\.0-9]+)/', $status, $cpu);
-        // grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
-        $smarty->assign('cpu_percent', round(100 * $cpu[2][0] / $cpu[1][0], 2));
-        $smarty->assign('cpu_load1', $cpu[2][0]);
-        $smarty->assign('cpu_load2', $cpu[3][0]);
-        $smarty->assign('cpu_load3', $cpu[4][0]);
+        if ($pveConnection > 0)
+        {
+          preg_match_all('/pve cpu thread (\d+) loadavg ([\.0-9]+) ([\.0-9]+) ([\.0-9]+)/', $status, $cpu);
+          // grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
+          $smarty->assign('cpu_percent', round(100 * $cpu[2][0] / $cpu[1][0], 2));
+          $smarty->assign('cpu_load1', $cpu[2][0]);
+          $smarty->assign('cpu_load2', $cpu[3][0]);
+          $smarty->assign('cpu_load3', $cpu[4][0]);
 
 
-        preg_match_all('/pve mem ram ([\.0-9]+) \w+ ([\.0-9]+) \w+ ([\.0-9]+)%/', $status, $mem);
-        $smarty->assign('mem_percent', $mem[3][0]);
-        $smarty->assign('mem_used', $mem[1][0]);
-        $smarty->assign('mem_total', $mem[2][0]);
+          preg_match_all('/pve mem ram ([\.0-9]+) \w+ ([\.0-9]+) \w+ ([\.0-9]+)%/', $status, $mem);
+          $smarty->assign('mem_percent', $mem[3][0]);
+          $smarty->assign('mem_used', $mem[1][0]);
+          $smarty->assign('mem_total', $mem[2][0]);
 
-        preg_match_all("/pve storage {$vars['Default Storage Engine']} ([\.0-9]+) \w+ ([\.0-9]+) \w+ ([\.0-9]+)%/", $status, $storage);
-        $smarty->assign('storage_percent', $storage[3][0]);
-        $smarty->assign('storage_used', $storage[1][0]);
-        $smarty->assign('storage_total', $storage[2][0]);
-        $smarty->assign('storage_engine', $vars['Default Storage Engine']);
+          preg_match_all("/pve storage {$vars['Default Storage Engine']} ([\.0-9]+) \w+ ([\.0-9]+) \w+ ([\.0-9]+)%/", $status, $storage);
+          $smarty->assign('storage_percent', $storage[3][0]);
+          $smarty->assign('storage_used', $storage[1][0]);
+          $smarty->assign('storage_total', $storage[2][0]);
+          $smarty->assign('storage_engine', $vars['Default Storage Engine']);
+        }
 
-        ksort($vars);
+        unset($vars['_lang']);
         unset($vars['smarty']);
         unset($vars['smartybc']);
         unset($vars['PVE Password']);
+        ksort($vars);
+
         $smarty->assign('configPVE', $vars);
 
         $paiditems_query = Capsule::table('tblinvoiceitems')
                                 ->where('tblinvoiceitems.status', 'Paid')
                                 ->where('tblinvoiceitems.notes', 'Managed by Proxmox addon')
                                 ->join('tblclients', 'tblclients.id', '=', 'tblinvoiceitems.userid')
-                                ->select(Capsule::raw('CONCAT(tblclients.firstname, \' \', tblclients.lastname) as username, tblinvoiceitems.id, tblinvoiceitems.invoiceid, tblinvoiceitems.userid, tblinvoiceitems.type, tblinvoiceitems.description, DATE_FORMAT(`updated_on`, \'%b %d, %Y %k:%i:%s\') as updated_on, DATE_FORMAT(`updated_on` + TIME(\'00:05\'), \'%b %d, %Y %k:%i:%s\') as time_expired, tblinvoiceitems.status'))
+                                ->select(Capsule::raw('CONCAT(tblclients.firstname, \' \', tblclients.lastname) as username, tblinvoiceitems.id, tblinvoiceitems.invoiceid, tblinvoiceitems.userid, tblinvoiceitems.type, tblinvoiceitems.description, DATE_FORMAT(`tblinvoiceitems`.`updated_at`, \'%b %d, %Y %k:%i:%s\') as updated_at, tblinvoiceitems.status'))
                                 ->get();
         // Convert object to associate array
         $paiditems = json_decode(json_encode($paiditems_query), TRUE);
-
-        $smarty->assign('invoiceitems', $paiditems);
+        $smarty->assign('paiditems', $paiditems);
 
 
         $queueditems_query = Capsule::table('tblinvoiceitems')
@@ -76,29 +85,20 @@ class Controller {
                                 ->orWhere('tblinvoiceitems.status', 'Creating')
                                 ->where('tblinvoiceitems.notes', 'Managed by Proxmox addon')
                                 ->join('tblclients', 'tblclients.id', '=', 'tblinvoiceitems.userid')
-                                ->select(Capsule::raw('CONCAT(tblclients.firstname, \' \', tblclients.lastname) as username, tblinvoiceitems.id, tblinvoiceitems.invoiceid, tblinvoiceitems.userid, tblinvoiceitems.type, tblinvoiceitems.description, DATE_FORMAT(`updated_on`, \'%b %d, %Y %k:%i:%s\') as updated_on, tblinvoiceitems.status'))
+                                ->select(Capsule::raw('CONCAT(tblclients.firstname, \' \', tblclients.lastname) as username, tblinvoiceitems.id, tblinvoiceitems.invoiceid, tblinvoiceitems.userid, tblinvoiceitems.type, tblinvoiceitems.description, DATE_FORMAT(`tblinvoiceitems`.`updated_at`, \'%b %d, %Y %k:%i:%s\') as updated_at, tblinvoiceitems.status'))
                                 ->get();
-        // Convert object to associate array
         $queueditems = json_decode(json_encode($queueditems_query), TRUE);
-
         $smarty->assign('queueditems', $queueditems);
-
-        logActivity(json_encode($queueditems));
-
-
-
 
         $createditems_query = Capsule::table('tblinvoiceitems')
                                 ->orWhere('tblinvoiceitems.status', 'Created')
                                 ->where('tblinvoiceitems.notes', 'Managed by Proxmox addon')
                                 ->join('tblclients', 'tblclients.id', '=', 'tblinvoiceitems.userid')
-                                ->select(Capsule::raw('CONCAT(tblclients.firstname, \' \', tblclients.lastname) as username, tblinvoiceitems.id, tblinvoiceitems.invoiceid, tblinvoiceitems.userid, tblinvoiceitems.type, tblinvoiceitems.description, DATE_FORMAT(`updated_on`, \'%b %d, %Y %k:%i:%s\') as updated_on, tblinvoiceitems.status'))
+                                ->select(Capsule::raw('CONCAT(tblclients.firstname, \' \', tblclients.lastname) as username, tblinvoiceitems.id, tblinvoiceitems.invoiceid, tblinvoiceitems.userid, tblinvoiceitems.type, tblinvoiceitems.description, DATE_FORMAT(`tblinvoiceitems`.`updated_at`, \'%b %d, %Y %k:%i:%s\') as updated_at, tblinvoiceitems.status'))
                                 ->get();
         // Convert object to associate array
         $createditems = json_decode(json_encode($createditems_query), TRUE);
         $smarty->assign('createditems', $createditems);
-
-        logActivity(json_encode($createditems));
 
         $smarty->display(dirname(__FILE__) . '/../../templates/admin/index.tpl');
 
@@ -149,5 +149,22 @@ EOF;
     {
       $smarty = $vars['smarty'];
       $smarty->display(dirname(__FILE__) . '/../../templates/admin/test.tpl');
+    }
+
+    public function create($vars)
+    {
+      $id = intval($_GET['id']);
+      $ipaddress = $_GET['ipaddress'];
+
+      if (!$this->whmcs->checkItem($id, 'Paid'))
+      {
+        //echo json_encode(['status' => 'fail', 'reason' => 'Invalid invoice item']);
+        http_response_code(404);
+      } else {
+        $this->whmcs->moveItemToQueue($id, $ipaddress);
+        echo json_encode(['status' => 'success']);
+      }
+
+      die();
     }
 }
